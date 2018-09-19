ad089766a3d336474a5c6c8c8952aeb6e8b4ee29
7etetic <7etetic@gmail.com>
2018-09-19 12:42:13 +0400

    Remove sudo from installation scripts

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -2,9 +2,8 @@
 
 # Install Ruby & dependences
 echo "Installing Ruby ..."
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
+apt update
+apt install -y ruby-full ruby-bundler build-essential
 echo "Finished installation Ruby"
-ruby -v && bundle -v
 
 
