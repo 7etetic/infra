9541dfff46dc61dc7715d3f8926cc0437504d05b
7etetic <7etetic@gmail.com>
2018-09-21 17:37:04 +0400

    Defined properties for VM instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,23 @@ provider "google" {
   project = "infra-216911"
   region = "europe-west1"
 }
+
+// Define VM resource
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "uerope-west1-b"
+  // Define boot disk
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-1537535643"
+    }
+  }
+  // Define network interface
+  network_interface {
+     // Connection network for interface
+     network = "default"
+     // Use ephemerial IP for an access from Internet
+    access_config {}
+  }
+}
