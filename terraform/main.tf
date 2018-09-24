9df47f9ceae330c87dedc6e1bec803fe7b01f200
7etetic <7etetic@gmail.com>
2018-09-24 19:03:04 +0400

    Added variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 provider "google" {
-  project = "infra-216911"
-  region = "europe-west1"
+  project = "${var.project}"
+  region = "${var.region}"
 }
 
 // Define VM resource
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
-  zone         = "europe-west1-b"
+  zone         = "${var.zone}"
   tags         = ["reddit-app"]
   // Define boot disk
   boot_disk {
     initialize_params {
-      image = "reddit-base-1537535643"
+      image = "${var.disk_image}"
     }
   }
   // Define network interface
@@ -24,13 +24,13 @@ resource "google_compute_instance" "app" {
   }
   // Add SSH key
   metadata {
-    sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
+    sshKeys = "appuser:${file("${var.public_key_path}")}"
   }
   connection {
     type        = "ssh"
     user        = "appuser"
     agent       = false
-    private_key = "${file("~/.ssh/appuser")}"
+    private_key = "${file("${var.private_key_path}")}"
   }
   // Define provisioners
   provisioner "file" {
